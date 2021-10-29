dd8ec91c4b49c3479e9f8ee5b44d362936d9c31e
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-10-30 00:20:31 +0600

    basic firebase setup done

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,6 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase.config";
+const firebaseInitialize = () => {
+    initializeApp(firebaseConfig);
+};
+export default firebaseInitialize;
