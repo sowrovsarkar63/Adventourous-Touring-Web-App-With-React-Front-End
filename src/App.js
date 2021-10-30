db7f162a935c9d3f3b16d0306443da225ae52682
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-10-30 10:13:05 +0600

    Ready start on this project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Pages/Home/Home";
 
 function App() {
     return (
-        <div className="App">
-            <h2>Application is running </h2>
-        </div>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <Home></Home>
+                </Route>
+                <Route path="/home">
+                    <Home></Home>
+                </Route>
+            </Switch>
+        </BrowserRouter>
     );
 }
 
---
+++ src/components/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../Header/Navbar/Navbar";
+
+const Home = () => {
+    return (
+        <>
+            <Navbar></Navbar>
+        </>
+    );
+};
+
+export default Home;
