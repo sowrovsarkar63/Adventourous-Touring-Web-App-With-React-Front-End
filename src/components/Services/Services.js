1c066f7ca9ec2c9ffe67e2ec5df5b5e3dcf0b039
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-10-31 11:04:03 +0600

    Touring places showing from the database

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,7 +1,19 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import "./Services.css";
 export default function Services() {
+    const [tourPlaces, setTourPlaces] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("https://adeventure-toure.herokuapp.com/toures")
+            .then((response) => {
+                const data = response.data;
+                setTourPlaces(data);
+            });
+    }, []);
+
     return (
         <Container className="mb-4">
             <div className=" mx-auto w-25  mt-4 pt-4 mb-4 pb-4 border border-warning border-top-0 border-bottom-0">
@@ -12,152 +24,24 @@ export default function Services() {
                 </h2>
             </div>
             <Row xs={1} md={2} lg={3} xl={3} className="g-4">
-                <Col>
-                    <Card className="shadow-lg card-style">
-                        <Card.Img
-                            variant="top"
-                            src="https://i.ibb.co/wJSFpys/service1resize.png"
-                        />
-                        <Card.Body className="font-monospace text-white">
-                            <Card.Title>Nepal Bali , Yugn</Card.Title>
-                            <Card.Text className="font-monospace text-white">
-                                Lorem ipsum dolor sit amet consectetur,
-                                adipisicing elit. Architecto, nihil et! Amet,
-                                cupiditate. Ducimus voluptatem quo deleniti
-                                aliquam odit minima sint culpa perspiciatis,
-                                optio est delectus, nostrum officia maiores
-                                quisquam.
-                            </Card.Text>
-                        </Card.Body>
-                        <div className="m-3 p-3 ">
-                            <Button variant="outline-warning" size="lg">
-                                Book Now
-                            </Button>
-                        </div>
-                    </Card>
-                </Col>
-
-                {/* service section */}
-                <Col>
-                    <Card className="shadow-lg card-style">
-                        <Card.Img
-                            variant="top"
-                            src="https://i.ibb.co/wJSFpys/service1resize.png"
-                        />
-                        <Card.Body className="font-monospace text-white">
-                            <Card.Title>Nepal Bali , Yugn</Card.Title>
-                            <Card.Text>
-                                Lorem ipsum dolor sit amet consectetur,
-                                adipisicing elit. Architecto, nihil et! Amet,
-                                cupiditate. Ducimus voluptatem quo deleniti
-                                aliquam odit minima sint culpa perspiciatis,
-                                optio est delectus, nostrum officia maiores
-                                quisquam.
-                            </Card.Text>
-                        </Card.Body>
-                        <div className="m-3 p-3 ">
-                            <Button variant="outline-warning" size="lg">
-                                Book Now
-                            </Button>
-                        </div>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card className="shadow-lg card-style">
-                        <Card.Img
-                            variant="top"
-                            src="https://i.ibb.co/wJSFpys/service1resize.png"
-                        />
-                        <Card.Body className="font-monospace text-white">
-                            <Card.Title>Nepal Bali , Yugn</Card.Title>
-                            <Card.Text className="font-monospace text-white">
-                                Lorem ipsum dolor sit amet consectetur,
-                                adipisicing elit. Architecto, nihil et! Amet,
-                                cupiditate. Ducimus voluptatem quo deleniti
-                                aliquam odit minima sint culpa perspiciatis,
-                                optio est delectus, nostrum officia maiores
-                                quisquam.
-                            </Card.Text>
-                        </Card.Body>
-                        <div className="m-3 p-3 ">
-                            <Button variant="outline-warning" size="lg">
-                                Book Now
-                            </Button>
-                        </div>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card className="shadow-lg-lg card-style">
-                        <Card.Img
-                            variant="top"
-                            src="https://i.ibb.co/wJSFpys/service1resize.png"
-                        />
-                        <Card.Body className="font-monospace text-white">
-                            <Card.Title>Nepal Bali , Yugn</Card.Title>
-                            <Card.Text className="font-monospace text-white">
-                                Lorem ipsum dolor sit amet consectetur,
-                                adipisicing elit. Architecto, nihil et! Amet,
-                                cupiditate. Ducimus voluptatem quo deleniti
-                                aliquam odit minima sint culpa perspiciatis,
-                                optio est delectus, nostrum officia maiores
-                                quisquam.
-                            </Card.Text>
-                        </Card.Body>
-                        <div className="m-3 p-3 ">
-                            <Button variant="outline-warning" size="lg">
-                                Book Now
-                            </Button>
-                        </div>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card className="shadow-lg card-style">
-                        <Card.Img
-                            variant="top"
-                            src="https://i.ibb.co/wJSFpys/service1resize.png"
-                        />
-                        <Card.Body className="font-monospace text-white">
-                            <Card.Title>Nepal Bali , Yugn</Card.Title>
-                            <Card.Text className="font-monospace text-white">
-                                Lorem ipsum dolor sit amet consectetur,
-                                adipisicing elit. Architecto, nihil et! Amet,
-                                cupiditate. Ducimus voluptatem quo deleniti
-                                aliquam odit minima sint culpa perspiciatis,
-                                optio est delectus, nostrum officia maiores
-                                quisquam.
-                            </Card.Text>
-                        </Card.Body>
-                        <div className="m-3 p-3 ">
-                            <Button variant="outline-warning" size="lg">
-                                Book Now
-                            </Button>
-                        </div>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card className="shadow-lg card-style">
-                        <Card.Img
-                            variant="top"
-                            src="https://i.ibb.co/wJSFpys/service1resize.png"
-                        />
-                        <Card.Body className="font-monospace text-white">
-                            <Card.Title>Nepal Bali , Yugn</Card.Title>
-                            <Card.Text className="font-monospace text-white">
-                                Lorem ipsum dolor sit amet consectetur,
-                                adipisicing elit. Architecto, nihil et! Amet,
-                                cupiditate. Ducimus voluptatem quo deleniti
-                                aliquam odit minima sint culpa perspiciatis,
-                                optio est delectus, nostrum officia maiores
-                                quisquam.
-                            </Card.Text>
-                        </Card.Body>
-                        <div className="m-3 p-3 ">
-                            <Button variant="outline-warning" size="lg">
-                                Book Now
-                            </Button>
-                        </div>
-                    </Card>
-                </Col>
+                {tourPlaces.map((singlePlace) => (
+                    <Col key={singlePlace._id}>
+                        <Card className="shadow-lg card-style">
+                            <Card.Img variant="top" src={singlePlace.img} />
+                            <Card.Body className="font-monospace text-white">
+                                <Card.Title>{singlePlace.title}</Card.Title>
+                                <Card.Text className="font-monospace text-white mt-5">
+                                    {singlePlace.description.slice(0, 200)}
+                                </Card.Text>
+                            </Card.Body>
+                            <div className="m-3 p-3 ">
+                                <Button variant="outline-warning" size="lg">
+                                    Book Now
+                                </Button>
+                            </div>
+                        </Card>
+                    </Col>
+                ))}
             </Row>
             <br />
             <br />
